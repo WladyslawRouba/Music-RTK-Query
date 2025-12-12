9d79f48e4ec8b6d12b59a8730e8379644136937f
Wladyslaw Rouba <roubovlad@gmail.com>
2025-12-12 10:56:09 +0100

    Get,RTK Query commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/ui/App/App.tsx'
+import { Provider } from 'react-redux';
+import { store } from './app/model/store.ts'
+
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+      </Provider>
+
   </BrowserRouter>,
 )
